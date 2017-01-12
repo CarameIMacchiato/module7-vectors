687f0ac0ac9063291112e3e6efee80d879966af9
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-01-12 12:51:31 -0800

    Complete exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,21 +2,27 @@
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
 
+x <- 10:20
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
 
+y <- seq(21, 30) 
 
 # Create a vector `z` by combining the vectors `x` and `y`
 
+z <- c(x,y)
 
 # Create a variable `w` that contains the 11th element in your vector `z`
 
+w <- z[11]
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
 
+q <- c(2:5)
 
 # Create a variable `len` that is equal to the length of your vector `q`
 
+len <- q.length
 
 ### Bonus ###
 
